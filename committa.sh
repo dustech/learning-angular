fb0ffe6fe9797f3dad1113abd555ad432fc30f6c
Stefano Cerruti <stefano.cerruti@eng.it>
2023-10-05 17:21:49 +0200

    inizio il corso

--- committa.sh
+++ committa.sh
@@ -1,1 +1,10 @@
-git pull && git add . && git commit -m "Aggiornamento al `date`" && git push && git status
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    msg="Aggiornamento al `DATE`"
+  else
+    msg="$1"
+fi
+
+git pull && git add . && git commit -m "$msg" && git push && git status
