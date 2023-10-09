6d2424b5c8861d0cb904723cf72e2f59771490d8
Stefano Cerruti <dustech@SOVEREIGN.lan>
2023-10-09 23:25:50 +0200

    in line templates

--- my-bootstrap/src/app/server/server.component.ts
+++ my-bootstrap/src/app/server/server.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  //template: 'bongo nucleare',
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
