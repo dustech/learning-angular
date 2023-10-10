f2c12792e173b27cf6b4a7d31358b7bca6869490
dustech <stefano.cerruti@outlook.com>
2023-10-10 16:17:12 +0200

    property bindings con il componente

--- my-bootstrap/src/app/servers/servers.component.ts
+++ my-bootstrap/src/app/servers/servers.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+
+  private allowNewServer: boolean = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  getAllowNewServer(): boolean {
+    return this.allowNewServer;
+  }
 
 }
